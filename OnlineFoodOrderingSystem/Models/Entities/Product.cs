c377bea682dcfba85428bbd9fc24dc07a1b0628b
Farrukh <Farrukhjon.Kholmatov@hotmail.com>
2021-09-24 22:51:00 +0500

    first commit

---
+++ OnlineFoodOrderingSystem/Models/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineFoodOrderingSystem.Models.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public int FoodPlaceId { get; set; }
+        public FoodPlace Place { get; set; }
+        public ICollection<Feedback> Feedbacks { get; set; }       
+    }
+}
---
+++ OnlineFoodOrderingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineFoodOrderingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
